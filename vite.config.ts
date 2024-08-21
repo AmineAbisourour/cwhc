47a74e635d77a015d35a309a46a82c9e3b1e83dc
Amine Abisourour <amine.abisourour@gmail.com>
2024-08-21 01:40:22 +0100

    Fixing  vercel 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Adjust if deploying to a subdirectory
+  build: {
+    // This ensures correct paths for assets in production
+    outDir: "dist", // This is the default output folder
+  },
+});
